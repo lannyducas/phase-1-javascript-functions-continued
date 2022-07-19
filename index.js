f5df75dfaa6c1c04da2f6f26f10905c2041a5eb5
Danny <lannyducas@gmail.com>
2022-07-19 13:02:09 -0400

    commit tree e845a38a941efedbb3fd39a12be420eabc6714c0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun (activity ="roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}// code your solution here
+function mondayWork(activity='go to the office'){
+return `This Monday, I will ${activity}.`
+}
+/*function wrapAdjective(param="special"){
+return `You are ${param}!`
+}*/
+
+function wrapAdjective(wrap = '*'){
+    return function innerFunction (adjective ="special"){
+        return `You are ${wrap}${adjective}${wrap}!`;
+    }
+}
+wrapAdjective("%")('a dedicated programmer');
